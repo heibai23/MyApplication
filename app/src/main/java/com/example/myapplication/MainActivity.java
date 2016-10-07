51bb265b2dd5c15c4373fd3f27f36e2fece649e0
heibai23 <497503962@qq.com>
2016-10-07 15:05:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.myapplication;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    private TabLayout tabLayout;    //标题布局
+    private ViewPager viewPager;        //
+    private LayoutInflater inflater;
+
+    private View view1,view2,view3;     //页卡视图
+    private ArrayList<View> viewList;        //装载页卡视图的集合
+    private ArrayList<String>titleList;      //装载页卡标题的集合
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //初始化，将布局文件转化为View对象，且作为数据源
+        viewList=new ArrayList<>();
+        titleList=new ArrayList<>();
+        viewPager= (ViewPager) findViewById(R.id.mViewPager);
+
+
+
+        inflater=LayoutInflater.from(this);             //将view转换成一个布局文件
+        view1=inflater.inflate(R.layout.activity_main,null);
+        view2=inflater.inflate(R.layout.activity_main,null);
+        view3=inflater.inflate(R.layout.activity_main,null);
+
+        //添加页卡视图
+        viewList.add(view1);
+        viewList.add(view2);
+        viewList.add(view3);
+
+        //添加页卡标题
+        titleList.add("花卉");
+        titleList.add("盆栽");
+        titleList.add("商店");
+
+
+        tabLayout= (TabLayout) findViewById(R.id.mTabLayout);
+
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);       //设置Tab模式
+        tabLayout.addTab(tabLayout.newTab().setText(titleList.get(0)));
+        tabLayout.addTab(tabLayout.newTab().setText(titleList.get(1)));
+        tabLayout.addTab(tabLayout.newTab().setText(titleList.get(2)));
+
+
+
+
+
+
+    //适配器
+        MyPagerAdapter adapter=new MyPagerAdapter(viewList,titleList);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);//将TabLayout和ViewPager关联起来。
+        tabLayout.setTabsFromPagerAdapter(adapter);//给Tabs设置适配器
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MyPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.myapplication;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 灰尘 on 2016/10/1.
+ */
+public class MyPagerAdapter extends PagerAdapter{
+
+    private ArrayList<View> viewList;
+    private ArrayList<String>titleList;
+
+    public MyPagerAdapter(ArrayList<View>viewList,ArrayList<String>titleList){
+
+        this.viewList=viewList;
+        this.titleList=titleList;
+    }
+
+    //实例化一个页卡
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(viewList.get(position));
+        return viewList.get(position);
+    }
+
+    //销毁一个页卡
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(viewList.get(position));
+    }
+
+    //页卡的数量
+    @Override
+    public int getCount() {
+        return viewList.size();
+    }
+
+    //View是否来自于对象
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+
+        return view==object;
+    }
+
+    public CharSequence getPageTitle(int position){
+        return titleList.get(position);
+    }
+
+
+
+
+}
